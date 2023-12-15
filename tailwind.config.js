77706babc87e3b107d63f5a6a7e2e7935f075e65
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2023-12-15 08:41:11 +0500

    tailwind_css file

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['./*.html', './pages/*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        colorFirst: '#F2CEDA',
+        colorSecond: '#D23166',
+        colorThird: '#0F0101',
+        colorFourth: '#150101',
+        colorFifth: '#fafafa',
+        colorsix: ' #D23166'
+      },
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      Fredoke: ['Fredoka One', 'sans-serif']
+    },
+  },
+  plugins: [],
+};
